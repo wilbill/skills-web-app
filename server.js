21c1e71b9ace392138ddce2adb8d6ac883ce2657
wilbill <ntabazibilly57@gmail.com>
2023-11-19 10:22:07 -0600

    done index

---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const express = require('express')
+const ejs = require('ejs');
+const app= express();
+const cookieParser = require('cookie-parser')
+
+app.set('view engine', 'ejs')
+// app.set('', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.get('/', function(req, res, next){
+    res.render('index');
+})
+
+app.get('/pages/billing', function(req, res){
+    res.render('pages/billing');
+})
+
+app.get('/pages/dashboard', function(req, res){
+    res.render('pages/dashboard')
+})
+
+app.get('/pages/profile', function(req, res){
+    res.render('pages/profile')
+})
+
+app.get('/pages/sign-in', function(req, res){
+    res.render('pages/sign-in')
+})
+
+app.get('/pages/sign-up', function(req, res){
+    res.render('pages/sign-up')
+})
+
+app.get('/pages/tables', function(req, res){
+    res.render('pages/tables')
+})
+
+app.get('/pages/rtl', function(req, res){
+    res.render('pages/rtl')
+})
+
+app.listen(80, function(req, res, next){
+    console.log('Server running on port 80');
+})
+
